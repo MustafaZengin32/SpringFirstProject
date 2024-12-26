4a50d2be7b889696519c10c8d91781c26772c8c8
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2024-12-26 13:12:21 +0100

    first

---
+++ src/main/java/com/SpringFirst/main/FirstClass.java
@@ -0,0 +1,9 @@
+package com.SpringFirst.main;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Spring World!");
+    }
+}
---
+++ src/main/java/com/SpringFirstMustafaZengin/SpringProjectMusti/SpringProjectMustiApplication.java
@@ -0,0 +1,16 @@
+package com.SpringFirstMustafaZengin.SpringProjectMusti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectMustiApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(SpringProjectMustiApplication.class, args);
+
+	}
+
+}
